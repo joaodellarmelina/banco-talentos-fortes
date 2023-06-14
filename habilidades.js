f20fe92b83ab956054c2b230dfabaf13dacbf11a
Glitch (glitch-hello-website) <none>
2023-06-14 12:34:57 +0000

    🐪⛈ Checkpoint ./habilidades.js:56529911/220 ./habilidades.css:56529911/1030

--- habilidades.js
+++ habilidades.js
@@ -1,2 +1,7 @@
-const habilidades
+const habilidadeSpan = document.querySelector('.habilidades-itens div');
 const removeBtn = document.querySelector('.habilidades-itens a');
+
+
+habilidadeSpan.addEventListener('mouseup', function() {
+  removeBtn.style.display = 'block';
+})
