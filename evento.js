fb9c32bf06800f1804f62a5bcc120b72a9bb7236
Glitch (glitch-hello-website) <none>
2023-06-16 20:52:12 +0000

    🎄🍀 Checkpoint ./home.css:56529911/406:57092472/327:58450370/89 ./script-home.js:57092472/310 ./eventos.html:57092472/62:58450370/78 ./eventos.css:57092472/324 ./evento.js:57092472/307 ./habilidades.js:57092472/956 ./habilidades.css:57092472/322 ./habilidades.html:57092472/63 ./treinamentos.js:57092472/310 ./treinamentos.html:57092472/62 ./treinamentos.css:57092472/326

---
+++ evento.js
@@ -0,0 +1,19 @@
+#btnTopo {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 60px;
+  height: 60px;
+  background-color: #7750ee;
+  color: #fff;
+  border: none;
+  border-radius: 50%;
+  opacity: 0.8;
+  cursor: pointer;
+  transition: opacity 0.3s, visibility 0.3s;
+}
+
+#btnTopo:hover {
+  opacity: 1;
+}
--- script-home.js
+++ script-home.js
@@ -13,3 +13,18 @@ closed.addEventListener("click", () => {
 
 });
 
+window.addEventListener('scroll', function () {
+  var btnTopo = document.getElementById('btnTopo');
+  if (window.scrollY > 20) {
+    btnTopo.style.display = 'block';
+  } else {
+    btnTopo.style.display = 'none';
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
