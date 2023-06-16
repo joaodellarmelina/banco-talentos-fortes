23954efb7b6a7139276b6976b8f5cb58e507967b
Glitch (glitch-hello-website) <none>
2023-06-16 11:09:56 +0000

    🍕🚇 Checkpoint ./perfil.css:58450370/377 ./habilidades.js:56529911/1312 ./perfil.html:58450370/126 ./habilidades.html:56529911/2126 ./habilidades.css:56529911/1974

--- habilidades.js
+++ habilidades.js
@@ -2,17 +2,10 @@ const habilidadesItens = document.querySelectorAll(".habilidades-itens");
 const habilidadesItensDiv = document.querySelector(".habilidades-itens div");
 const removeBtn = document.querySelectorAll(".remove-button");
 const addBtn = document.querySelectorAll(".btn-add");
-const habilidadesBlur = document.querySelector("#blur");
-const inputContainer = document.querySelector(".input-container");
-const btnSalvar = document.querySelector(".salvar");
-const btnCancelar = document.querySelector(".cancelar");
-const form = document.querySelector(".form-group");
-const inputField = document.querySelector(".form-field");
-var inputValue;
 var newHabilidade = (document.createElement = `
                 <div class="habilidades-itens">
                   <div>
-                    <span>${inputValue}</span>
+                    
                   </div>
                   <a class="remove-button">
                     <i class="fa-solid fa-xmark"></i>
@@ -41,36 +34,3 @@ removeBtn.forEach((e) => {
   });
 });
 
-addBtn.forEach((e) => {
-  e.addEventListener("click", function () {
-    habilidadesBlur.classList.toggle("active");
-    inputContainer.style.display = "flex";
-  });
-});
-
-function submitForm() {
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    form.reset();
-    inputValue = inputField.value;
-  });
-}
-
-inputField.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    submitForm();
-  }
-});
-
-btnSalvar.addEventListener("click", function (e) {
-  submitForm();
-  inputContainer.style.display = "none";
-  habilidadesBlur.classList.toggle("active");
-});
-
-btnCancelar.addEventListener("click", function () {
-  inputContainer.style.display = "none";
-  habilidadesBlur.classList.toggle("active");
-});
-
-console.log(newHabilidade);
