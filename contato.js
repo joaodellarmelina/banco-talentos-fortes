1ed8711c0fe49a74367417efe7c8964627bdeb38
Glitch (glitch-hello-website) <none>
2023-06-16 21:15:28 +0000

    🏆🚍 Checkpoint ./confraternizacao.css:56529911/4508 ./treinamentos.css:58450370/96 ./contato.js:57092472/1:58451237/1341 ./confraternizacao.html:56529911/13619 ./contato.html:57092472/62:58451237/13238 ./contato.css:57092472/326:58450370/282:58451237/11112 ./treinamentos.html:58450370/57 ./eventos.css:58450370/124

--- contato.js
+++ contato.js
@@ -1,17 +1,3 @@
-const click = document.querySelector("#icone-notifi");
-const bloco_notifi = document.getElementById("bloco-notifi")
-const closed = document.getElementById("close")
-
-click.addEventListener("click", () => {
-    bloco_notifi.style.display = "block";
-    closed.style.display = "block";
-});
-
-closed.addEventListener("click", () => {
-    bloco_notifi.style.display = "none";
-    closed.style.display = "none";
-
-});
 
 
 //BackToTop
